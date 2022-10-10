83e3e322e020bb9982c5d154fc7f454b6e819e3f
DIGIKRASH <digitallkrash@gmail.com>
2022-10-10 18:01:10 +0600

    переделка дизайна

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,22 @@
-$('.item').click(function () {
-  $('.item-popup').toggleClass('active');
+$(".item").click(function () {
+  $(".item-popup").toggleClass("active");
 });
 
-$('.item__close').click(function () {
-  $('.item-popup').removeClass('active');
+$(".item__close").click(function () {
+  $(".item-popup").removeClass("active");
 });
 
+if ($(".item__title").height() > 50) {
+  $(this).toggleClass("mini");
+}
+
+$(".item").each(function () {
+  var $min = 30;
+  if ($(this).find('.item__title').height() > $min) {
+    $(this).find('.item__title').addClass("mini");
+  }
+});
+
+$('.sidebar__list-item').click(function () {
+  $(this).toggleClass('active');
+});
