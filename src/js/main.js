07eaf09cc3741acbe4b1867a2809e7c463895130
DIGIKRASH <digitallkrash@gmail.com>
2022-10-11 17:43:59 +0600

    оставшееся

--- src/js/main.js
+++ src/js/main.js
@@ -12,15 +12,51 @@ if ($(".item__title").height() > 50) {
 
 $(".item").each(function () {
   var $min = 30;
-  if ($(this).find('.item__title').height() > $min) {
-    $(this).find('.item__title').addClass("mini");
+  if ($(this).find(".item__title").height() > $min) {
+    $(this).find(".item__title").addClass("mini");
   }
 });
 
-$('.sidebar__list-item').click(function () {
-  $(this).toggleClass('active');
+$(".sidebar__list-item").click(function () {
+  $(this).toggleClass("active");
 });
 
-$('.item__add-element').click(function () {
-  $(this).toggleClass('active');
+$(".item__add-element").click(function () {
+  $(this).toggleClass("active");
 });
+
+const btnUp = {
+  el: document.querySelector(".btn-up"),
+  show() {
+    // удалим у кнопки класс btn-up_hide
+    this.el.classList.add("show");
+  },
+  hide() {
+    // добавим к кнопке класс btn-up_hide
+    this.el.classList.remove("show");
+  },
+  addEventListener() {
+    // при прокрутке содержимого страницы
+    window.addEventListener("scroll", () => {
+
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+      var block_position = $("footer").offset().top;
+
+      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+      scrollY > 400 ? this.show() : this.hide();
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector(".btn-up").onclick = () => {
+      // переместим в начало страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    };
+  },
+};
+
+btnUp.addEventListener();
